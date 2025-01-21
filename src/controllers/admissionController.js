567e0c2969b1408c37d7f8a2a326359582c5983c
Ashutosh Dhakad <ashutoshatoliya@gmail.com>
2025-01-21 13:04:57 +0530

    add some more configurations in the admission controllers

--- src/controllers/admissionController.js
+++ src/controllers/admissionController.js
@@ -1,4 +1,5 @@
 import createHttpError from "http-errors";
+// import cloudinary from ""
 import Admission from "../models/admissionModel.js";
 import { Counter } from "../models/counterModel.js";
 
@@ -98,5 +99,56 @@ const createAdmission = async (req, res, next) => {
     //    .render("error", { message: "Server Error while creating admission." });
   }
 };
+
+const allAdmissions = async (req, res, next) => {
+  try {
+    const admissions = await Admission.find(); // Fetch all admissions
+    const formattedAdmissions = admissions.map((admission) => {
+      // Format DOB to a human-readable string
+      admission.DOB = admission.DOB.toLocaleDateString();
+      return admission;
+    });
+    res.json({ success: true, admissions: formattedAdmissions });
+  } catch (error) {
+    next(createHttpError(500, "Server Error while fetching admissions."));
+  }
+};
+
+const editAdmission = async (req, res, next) => {
+  try {
+    const { id: admissionId } = req.params;
+    let updatedData = req.body;
+
+    console.log("Admission ID:", admissionId);
+    console.log("Updated Data:", updatedData);
+
+    if (req.file) {
+      const result = await cloudinary.uploader.upload(req.file.path, {
+        folder: "admissions",
+      });
+
+      updatedData.uploadPhoto = result.secure_url;
+    }
+
+    const admission = await Admission.findOneAndUpdate(
+      { admissionId },
+      updatedData,
+      { new: true }
+    );
+
+    if (!admission) {
+      return next(createHttpError(404, "Admission not found"));
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Admission updated successfully",
+      admission,
+    });
+  } catch (err) {
+    console.error("Error in editAdmission:", err);
+    next(createHttpError(500, "Server Error while updating admission"));
+  }
+};
   
-export { createAdmission };
+export { createAdmission, allAdmissions, editAdmission };
--- src/routes/admissionRouter.js
+++ src/routes/admissionRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
 import upload from "../middlewares/uploadMiddleware.js";
-import { createAdmission } from "../controllers/admissionController.js";
+import { allAdmissions, createAdmission, editAdmission } from "../controllers/admissionController.js";
 
 const router = express.Router();
 
 // Admission creation route
 router.post("/create", upload.single("uploadPhoto"), createAdmission);
+router.get("/list", allAdmissions);
+router.put("/:id", upload.single("uploadPhoto"), editAdmission);
 
 export default router;
